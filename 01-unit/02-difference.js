d27a5cc08673bef4b99d0e113983600827022862
Wendy Tokaryk <tokaryk@hotmail.com>
2020-11-27 16:36:28 -0600

    02-difference Tests 3&4 passed

--- 01-unit/02-difference.js
+++ 01-unit/02-difference.js
@@ -32,11 +32,16 @@ try {
   // Test Case 3
   // --------------------------------------------------
   // When invoked with only 1 number, it should return that number.
+  var result = difference(8);
+  if (result !== 8) throw new Error('Expected sum(8) to be 8. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // When invoked with no numbers, it should return 0.
+  var result = difference ();
+  if (result !== 0) throw new Error('Expected sum() to be 0. Received: ' + result);
+
 
   console.log('All tests passed successfully.');
 
