6de10ed6c55ff0aa47370d2c90f86034b6093de9
Wendy Tokaryk <tokaryk@hotmail.com>
2020-11-27 16:42:23 -0600

    03-product Tests1&2 passed

--- 01-unit/03-product.js
+++ 01-unit/03-product.js
@@ -1,8 +1,12 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function product() {
-
+function product(num1, num2) {
+  if (num1 === undefined) num1 = 0;
+  if (num2 === undefined) num2 = 0;
+  
+  var $result= num1*num2
+  return $result
 }
 
 // ==================================================
@@ -13,14 +17,16 @@ try {
   // Test Case 1
   // --------------------------------------------------
   // It should return the product of two numbers.
-  var result = product(2, 2);
-  if (result !== 4) throw new Error('Expected product(2, 2) to be 4. Received: ' + result);
+  var result = product(2, 3);
+  if (result !== 6) throw new Error('Expected product(2, 3) to be 6. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 2
   // --------------------------------------------------
   // It should return 0 when either of the numbers are 0.
-
+  var result = product(0, 3);
+  if (result !== 0) throw new Error('Expected product(0, 3) to be 0. Received: ' + result);
+  
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
